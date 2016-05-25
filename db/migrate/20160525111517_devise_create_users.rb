72b052ab2ee751d599d42e45f6f8466699de7c69
yonggoonoh <nohyg90@gmail.com>
2016-05-25 20:21:16 +0900

    Migrate user db

--- db/migrate/20160525111517_devise_create_users.rb
+++ db/migrate/20160525111517_devise_create_users.rb
@@ -4,6 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+			t.string :user_name, null: false
+			t.string :phone_number, null: false
 
       ## Recoverable
       t.string   :reset_password_token

966c57c8b237aae329fa7cf27fb44cc6872db736
yonggoonoh <nohyg90@gmail.com>
2016-05-26 15:47:10 +0900

    Change devise model constraint user_name, phone_number

--- db/migrate/20160525111517_devise_create_users.rb
+++ db/migrate/20160525111517_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-			t.string :user_name, null: false
-			t.string :phone_number, null: false
+			t.string :user_name, null: false, default: ""
+			t.string :phone_number, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 20160525115654) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "user_name",                           null: false
-    t.string   "phone_number",                        null: false
+    t.string   "user_name",              default: "", null: false
+    t.string   "phone_number",           default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

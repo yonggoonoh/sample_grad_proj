4676fdb367206d430bdf456ff52231e79095909a
yonggoonoh <nohyg90@gmail.com>
2016-05-29 01:10:50 +0900

    Improve seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,25 +10,49 @@
 # user_id
 # 1: 노용구 2: 나현수 3: 안준엽 4: 이순신
 
+#User.create([
+#	{ id: 1, email: "nohyg@naver.com", user_name: "노용구", phone_number: "01030113115", password: "aaaabbbb", password_confirmation: "aaaabbbb" },
+#	{ id: 2, email: "nahs@naver.com", user_name: "나현수", phone_number: "01056692739", password: "aaaabbbb", password_confirmation: "aaaabbbb" },
+#	{ id: 3, email: "ahnjy@naver.com", user_name: "안준엽", phone_number: "01012318123", password: "aaaabbbb", password_confirmation: "aaaabbbb" },
+#	{ id: 4, email: "leess@naver.com", user_name: "이순신", phone_number: "01022233224", password: "aaaabbbb", password_confirmation: "aaaabbbb" }
+#])
+
+Relationship.create([
+	{ user_id: 1, contact_id: 1, fb_friend: true, degree: 8.0 },
+	{ user_id: 1, contact_id: 2, fb_friend: true, degree: 6.0 },
+	{ user_id: 1, contact_id: 3, fb_friend: false, degree: 8.0 },
+	{ user_id: 1, contact_id: 4, fb_friend: true, degree: 1.0 },
+	{ user_id: 1, contact_id: 5, fb_friend: false, degree: 5.0 },
+	{ user_id: 2, contact_id: 1, fb_friend: true, degree: 6.0 },
+	{ user_id: 2, contact_id: 2, fb_friend: false, degree: 4.0 },
+	{ user_id: 2, contact_id: 6, fb_friend: false, degree: 9.0 },
+	{ user_id: 2, contact_id: 7, fb_friend: true, degree: 2.0 },
+	{ user_id: 2, contact_id: 8, fb_friend: false, degree: 4.0 },
+	{ user_id: 3, contact_id: 6, fb_friend: true, degree: 9.0 },
+	{ user_id: 3, contact_id: 8, fb_friend: false, degree: 5.0 },
+	{ user_id: 3, contact_id: 9, fb_friend: true, degree: 7.0 },
+	{ user_id: 3, contact_id: 10, fb_friend: true, degree: 8.0 },
+	{ user_id: 3, contact_id: 11, fb_friend: false, degree: 6.0 },
+	{ user_id: 4, contact_id: 1, fb_friend: true, degree: 1.0 },
+	{ user_id: 4, contact_id: 11, fb_friend: false, degree: 4.0 },
+	{ user_id: 4, contact_id: 12, fb_friend: true, degree: 8.0 },
+	{ user_id: 4, contact_id: 13, fb_friend: false, degree: 5.0 },
+	{ user_id: 4, contact_id: 14, fb_friend: false, degree: 6.0 }
+])
+
 Contact.create([
-	{ user_id: 1, user_name: '안준엽', phone_number: '01012318123', email: 'ahnjy@naver.com', fb_friend: true },
-	{ user_id: 1, user_name: '이승종', phone_number: '01084237824', email: 'leesj@naver.com', fb_friend: true },
-	{ user_id: 1, user_name: '나현수', phone_number: '01056692739', email: 'nahs@naver.com', fb_friend: false },
-	{ user_id: 1, user_name: '강광훈', phone_number: '01048562922', email: 'kangkh@naver.com', fb_friend: true },
-	{ user_id: 1, user_name: '김기훈', phone_number: '01054665444', email: 'kimkh@naver.com', fb_friend: false },
-	{ user_id: 2, user_name: '안준엽', phone_number: '01012318123', email: 'ahnjy@naver.com', fb_friend: true },
-	{ user_id: 2, user_name: '이승종', phone_number: '01084237824', email: 'leesj@naver.com', fb_friend: false },
-	{ user_id: 2, user_name: '노용구', phone_number: '01030113115', email: 'nohyg@naver.com', fb_friend: false },
-	{ user_id: 2, user_name: '박성빈', phone_number: '01073337444', email: 'parksb@naver.com', fb_friend: true },
-	{ user_id: 2, user_name: '전성구', phone_number: '01058552324', email: 'jeonsg@naver.com', fb_friend: false },
-	{ user_id: 3, user_name: '노용구', phone_number: '01030113115', email: 'nohyg@naver.com', fb_friend: true },
-	{ user_id: 3, user_name: '전성구', phone_number: '01058552324', email: 'jeonsg@naver.com', fb_friend: false },
-	{ user_id: 3, user_name: '이순신', phone_number: '01022233224', email: 'leess@naver.com', fb_friend: true },
-	{ user_id: 3, user_name: '이채원', phone_number: '01047162592', email: 'leecw@naver.com', fb_friend: true },
-	{ user_id: 3, user_name: '김지원', phone_number: '01057285275', email: 'kimjw@naver.com', fb_friend: false },
-	{ user_id: 4, user_name: '안준엽', phone_number: '01012318123', email: 'ahnjy@naver.com', fb_friend: true },
-	{ user_id: 4, user_name: '김지원', phone_number: '01057285275', email: 'kimjw@naver.com', fb_friend: false },
-	{ user_id: 4, user_name: '강감찬', phone_number: '01018492895', email: 'kangkc@naver.com', fb_friend: true },
-	{ user_id: 4, user_name: '스티브', phone_number: '01088575827', email: 'steve@naver.com', fb_friend: false },
-	{ user_id: 4, user_name: '이한수', phone_number: '01058279391', email: 'leehs@naver.com', fb_friend: false },
+	{ id: 1, user_name: '안준엽', phone_number: '01012318123', email: 'ahnjy@naver.com' },
+	{ id: 2, user_name: '이승종', phone_number: '01084237824', email: 'leesj@naver.com' },
+	{ id: 3, user_name: '나현수', phone_number: '01056692739', email: 'nahs@naver.com' },
+	{ id: 4, user_name: '강광훈', phone_number: '01048562922', email: 'kangkh@naver.com' },
+	{ id: 5, user_name: '김기훈', phone_number: '01054665444', email: 'kimkh@naver.com' },
+	{ id: 6, user_name: '노용구', phone_number: '01030113115', email: 'nohyg@naver.com' },
+	{ id: 7, user_name: '박성빈', phone_number: '01073337444', email: 'parksb@naver.com' },
+	{ id: 8, user_name: '전성구', phone_number: '01058552324', email: 'jeonsg@naver.com' },
+	{ id: 9, user_name: '이순신', phone_number: '01022233224', email: 'leess@naver.com' },
+	{ id: 10, user_name: '이채원', phone_number: '01047162592', email: 'leecw@naver.com' },
+	{ id: 11, user_name: '김지원', phone_number: '01057285275', email: 'kimjw@naver.com' },
+	{ id: 12, user_name: '강감찬', phone_number: '01018492895', email: 'kangkc@naver.com' },
+	{ id: 13, user_name: '스티브', phone_number: '01088575827', email: 'steve@naver.com' },
+	{ id: 14, user_name: '이한수', phone_number: '01058279391', email: 'leehs@naver.com' }
 ])

cd2f351d17b54bc32f1dd93364f6869de97ad56e
yonggoonoh <nohyg90@gmail.com>
2016-05-29 01:26:19 +0900

    Complete basic search search user by user_name and print it on /result

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,9 @@ class SearchController < ApplicationController
 	end
 
 	def result
-		@result = params[:user_name]
+		@result = Contact.where("user_name like ?", "%#{params[:user_name]}%")
+		if @result.empty?
+			@result = []
+		end
 	end
 end
